d8434d1b4aa0805067452b91f90c1b59774bc992
Camusat Antoine <antoine.camusat@insa-rouen.fr>
2022-11-25 21:26:19 -0500

    front: index to add a tracked address

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return <>
+        <Component {...pageProps} />
+        <ToastContainer
+            position="bottom-right"
+            autoClose={1500}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+        />
+    </>
 }
