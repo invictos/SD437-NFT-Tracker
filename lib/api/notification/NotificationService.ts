d5a6abfaa70357ae260ba38222faf4ed248dfd2a
zichzhang <zi.c.zhang@mail.mcgill.ca>
2022-11-26 01:07:17 -0500

    added send sms function with twilio

--- lib/api/notification/NotificationService.ts
+++ lib/api/notification/NotificationService.ts
@@ -1,7 +1,24 @@
 import { NotificationHandler, NotificationRequest } from "types/notification";
 
+const dotenv = require('dotenv').config()
+const twilio = require('twilio');
+
+const client = new twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
+
 export class NotificationService implements NotificationHandler{
     async sendNotification(notification: NotificationRequest): Promise<void> {
+        client.messages
+        .create({
+            body: `NFT at address ${notification.event.nft.contract.contractAddress} and token ${notification.event.nft.tokenId} was transfered from ${notification.event.from} to ${notification.event.to}`,
+            to: notification.event.from,
+            from: notification.event.to
+        })
+        .then(() => {
+            console.log('Message sent succesfully.')
+        }).catch((error: Error) => {
+            console.error(error);
+        });
+
         console.log('NOTIFICATION: ', notification);
         console.log(JSON.stringify(notification));
     }
