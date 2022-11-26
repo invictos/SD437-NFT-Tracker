2e97affe346e7a9c6662eae98cfaf40fad5b2ef4
zichzhang <zi.c.zhang@mail.mcgill.ca>
2022-11-26 12:38:04 -0500

    removed dotenv dependency

--- lib/api/notification/NotificationService.ts
+++ lib/api/notification/NotificationService.ts
@@ -1,18 +1,15 @@
 import { NotificationHandler, NotificationRequest } from "types/notification";
-import * as dotenv from "dotenv";
 import Twilio from "twilio";
 
-dotenv.config()
-
-const client = Twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
+const client = Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 export class NotificationService implements NotificationHandler{
     async sendNotification(notification: NotificationRequest): Promise<void> {
         client.messages
         .create({
             body: `NFT at address ${notification.event.nft.contract.contractAddress} and token ${notification.event.nft.tokenId} was transfered from ${notification.event.from} to ${notification.event.to}`,
-            to: notification.event.from,
-            from: notification.event.to
+            to: notification.receivers[0].payload,
+            from: process.env.TWILIO_FROM_PHONE_NUMBER
         })
         .then(() => {
             console.log('Message sent succesfully.')
