6eba0d7e89830d14348e6a7aecba446b91724a96
Zi Chao Zhang <zi.c.zhang@mail.mcgill.ca>
2022-11-26 19:28:09 -0500

    created send sms function with twilio api (#4)
    * ignore env files
    
    * added send sms function with twilio
    
    * fixed linter error
    
    * removed dotenv dependency

--- lib/api/notification/NotificationService.ts
+++ lib/api/notification/NotificationService.ts
@@ -1,7 +1,22 @@
 import { NotificationHandler, NotificationRequest } from "types/notification";
+import Twilio from "twilio";
+
+const client = Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 export class NotificationService implements NotificationHandler{
     async sendNotification(notification: NotificationRequest): Promise<void> {
+        client.messages
+        .create({
+            body: `NFT at address ${notification.event.nft.contract.contractAddress} and token ${notification.event.nft.tokenId} was transfered from ${notification.event.from} to ${notification.event.to}`,
+            to: notification.receivers[0].payload,
+            from: process.env.TWILIO_FROM_PHONE_NUMBER
+        })
+        .then(() => {
+            console.log('Message sent succesfully.')
+        }).catch((error: Error) => {
+            console.error(error);
+        });
+
         console.log('NOTIFICATION: ', notification);
         console.log(JSON.stringify(notification));
     }
