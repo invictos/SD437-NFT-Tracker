9c9c79cfd0d66bbbb376d148df61f08b6f2e09e0
zichzhang <zi.c.zhang@mail.mcgill.ca>
2022-12-02 00:07:05 -0500

    added send email notification feat with SendGrid

--- lib/api/notification/NotificationService.ts
+++ lib/api/notification/NotificationService.ts
@@ -1,15 +1,28 @@
 import Twilio from "twilio";
+import sgMail from '@sendgrid/mail';
+import SendGrid from "@sendgrid/client";
 import { NotificationHandler, NotificationRequest } from "types/notification";
 
 
 export class NotificationService implements NotificationHandler{
     twilioClient: Twilio.Twilio;
+    sgClient: SendGrid.Client;
 
     constructor() {
         this.twilioClient = Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+        sgMail.setClient(this.sgClient);
     }
 
     async sendNotification(notification: NotificationRequest): Promise<void> {
+        if (notification.receivers[0].receiverType === 'EMAIL') {
+            await this.sendEmail(notification);
+        } else {
+            await this.sendSMS(notification);
+        }
+    }
+
+    async sendSMS(notification: NotificationRequest): Promise<void> {
         this.twilioClient.messages
         .create({
             body: `NFT at address ${notification.event.nft.contract.contractAddress} and token ${notification.event.nft.tokenId} was transfered from ${notification.event.from} to ${notification.event.to}`,
@@ -21,8 +34,25 @@ export class NotificationService implements NotificationHandler{
         }).catch((error: Error) => {
             console.error(error);
         });
-
-        console.log('NOTIFICATION: ', notification);
-        console.log(JSON.stringify(notification));
     }
+
+    async sendEmail(notification: NotificationRequest): Promise<void> {
+        sgMail
+        .send({
+            to: notification.receivers[0].payload,
+            from: 'nftaddresstracker@gmail.com',
+            subject: 'Alert: New NFT transfer',
+            text: `NFT at address ${notification.event.nft.contract.contractAddress} and token ${notification.event.nft.tokenId} was transfered from ${notification.event.from} to ${notification.event.to}`,
+            html: `NFT at address ${notification.event.nft.contract.contractAddress} and token ${notification.event.nft.tokenId} was transfered from ${notification.event.from} to ${notification.event.to}`,
+        })
+        .then(() => {
+            console.log('Email sent succesfully.');
+        }, error => {
+            console.log(error);
+            if (error.response) {
+                console.error(error.response.body)
+            }
+        })
+    }
+
 }
